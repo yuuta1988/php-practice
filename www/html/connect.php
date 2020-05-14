b812df4fbcc5485e6586e3238372949d08d453be
Yuuta Sawamura <>
2020-05-14 09:22:19 +0900

    [update]

--- www/html/connect.php
+++ www/html/connect.php
@@ -4,16 +4,21 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+$service = getenv('DB_SERVICE');
+$name = getenv('DB_NAME');
+$usr = getenv('DB_USER');
+$passwd = getenv('DB_PASSWORD');
 
-// $dsn = 'mysql:dbname=php_practice; host=127.0.0.1; charset=utf8';
-// $usr = 'selfusr';
-// $passwd = 'selfpass';
-
-// try {
-//     $db = new PDO($dsn, $usr, $passwd);
-//     print '接続に成功しました。';
-// } catch (PDOException $e) {
-//     print "接続エラー:{$e->getMessage()}";
-// } finally {
-//     $db = null;
-// }
+$dsn = "mysql:host=$service;dbname=$name;charset=utf8mb4";
+try {
+    $db = new PDO($dsn, $usr, $passwd);
+    $sql = "SELECT * FROM member";
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    var_dump($result);
+} catch (PDOException $e) {
+    print "接続エラー:{$e->getMessage()}";
+} finally {
+    $db = null;
+}
